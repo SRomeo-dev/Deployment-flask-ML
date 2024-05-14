2cc0802ff5bc5d0d3bcf9fd96aa4917f8c301e5e
SRomeo-dev <sawadogoromeo03@gmail.com>
2024-05-14 12:11:46 +0000

    Version final

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import joblib
 
 app = Flask(__name__)
-model = joblib.load('model copy.pkl')
+model = joblib.load('model.pkl')
 
 @app.route('/')
 def home():
@@ -24,7 +24,7 @@ def predict():
         Total_S2 = request.form['Total_S2']
         Moyenne_S2 = request.form['Moyenne_S2']
         Resultat_S2 = request.form['Résultat_S2']
-        
+
         # Créer un dictionnaire pour stocker les valeurs valides
         valid_values = {}
 
@@ -34,50 +34,64 @@ def predict():
             error_messages['Mat'] = 'Le champ est requis'
         elif not 0 <= float(Mat) <= 20:
             error_messages['Mat'] = 'La note doit être comprise entre 0 et 20'
+        else:
+            valid_values['Mat'] = Mat
         
         if Mi1 == '':
             error_messages['Mi1'] = 'Le champ est requis'
         elif not 0 <= float(Mi1) <= 20:
             error_messages['Mi1'] = 'La note doit être comprise entre 0 et 20'
+        else:
+            valid_values['Mi1'] = Mi1
         
         if Total_S1 == '':
             error_messages['Total_S1'] = 'Le champ est requis'
         elif not 0 <= float(Total_S1) <= 600:
             error_messages['Total_S1'] = 'La note doit être comprise entre 0 et 600'
+        else:
+            valid_values['Total_S1'] = Total_S1
         
         if Moyenne_S1 == '':
             error_messages['Moyenne_S1'] = 'Le champ est requis'
         elif not 0 <= float(Moyenne_S1) <= 20:
             error_messages['Moyenne_S1'] = 'La note doit être comprise entre 0 et 20'
+        else:
+            valid_values['Moyenne_S1'] = Moyenne_S1
         
         if Resultat_S1 == '2':
             error_messages['Résultat_S1'] = 'Le champ est requis'
         else:
-            Resultat_S1 = (Resultat_S1)
+            valid_values['Résultat_S1'] = Resultat_S1
 
         if Mi2 == '':
             error_messages['Mi2'] = 'Le champ est requis'
         elif not 0 <= float(Mi2) <= 20:
             error_messages['Mi2'] = 'La note doit être comprise entre 0 et 20'
+        else:
+            valid_values['Mi2'] = Mi2
         
         if Total_S2 == '':
             error_messages['Total_S2'] = 'Le champ est requis'
         elif not 0 <= float(Total_S2) <= 600:
             error_messages['Total_S2'] = 'La note doit être comprise entre 0 et 600'
+        else:
+            valid_values['Total_S2'] = Total_S2
         
         if Moyenne_S2 == '':
             error_messages['Moyenne_S2'] = 'Le champ est requis'
         elif not 0 <= float(Moyenne_S2) <= 20:
             error_messages['Moyenne_S2'] = 'La note doit être comprise entre 0 et 20'
+        else:
+            valid_values['Moyenne_S2'] = Moyenne_S2
 
         if Resultat_S2 == '3':
             error_messages['Résultat_S2'] = 'Le champ est requis'
         else:
-            Resultat_S2 = (Resultat_S2)
+            valid_values['Résultat_S2'] = Resultat_S2
 
 
         if error_messages:
-            return render_template('pages-register.html', error_messages=error_messages)
+            return render_template('pages-register.html', error_messages=error_messages, valid_values=valid_values)
 
         # Convertir les valeurs en nombres flottants
         Mat = float(Mat)
@@ -91,7 +105,7 @@ def predict():
         Resultat_S2 = Resultat_S2
 
         # Concaténation des caractéristiques
-        final_features = [[Mat, Mi1, Total_S1, Moyenne_S1, Resultat_S1, Mi2, Total_S2, Moyenne_S2, Resultat_S2]]
+        final_features = [[Total_S1, Moyenne_S1, Resultat_S1, Total_S2, Moyenne_S2, Resultat_S2, Mat, Mi1, Mi2]]
         
         # Prédiction de probabilité
         probabilites_predites = model.predict_proba(final_features)
@@ -101,6 +115,11 @@ def predict():
 
         prediction_available = True
 
-        return render_template('pages-register.html', prediction_text='Votre chance de réussite: {} %'.format(pourcentage_predit), prediction_available=prediction_available)
+        
+        # Calculer la classe CSS en fonction de la valeur de pourcentage_predit
+        text_color_class = 'text-success' if pourcentage_predit >= 50 else 'text-danger'
+
+
+        return render_template('pages-register.html', prediction_text='Votre chance de réussite est de: {} %'.format(pourcentage_predit), prediction_available=prediction_available, text_color_class=text_color_class)
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,42 +1,42 @@
 // JavaScript pour désactiver la sélection de l'option "Choisissez une valeur"
 function disablePlaceholderOption1() {
-    var select = document.getElementById("selectValue");
-    var placeholderOption = select.options[0];
-    placeholderOption.disabled = true;
+  var select = document.getElementById("selectValue");
+  var placeholderOption = select.options[0];
+  placeholderOption.disabled = true;
 }
 
 // JavaScript pour désactiver la sélection de l'option "Choisissez une valeur"
 function disablePlaceholderOption2() {
-    var select = document.getElementById("selectValue2");
-    var placeholderOption = select.options[0];
-    placeholderOption.disabled = true;
+  var select = document.getElementById("selectValue2");
+  var placeholderOption = select.options[0];
+  placeholderOption.disabled = true;
 }
 // Fonction pour mettre à jour les moyennes et les résultats en fonction des totaux
 function updateForm() {
-    var totalS1 = parseFloat(document.getElementById("Total_S1").value);
-    var totalS2 = parseFloat(document.getElementById("Total_S2").value);
+  var totalS1 = parseFloat(document.getElementById("Total_S1").value);
+  var totalS2 = parseFloat(document.getElementById("Total_S2").value);
 
-    if (!isNaN(totalS1)) {
-        var moyenneS1 = totalS1 / 30;
-        document.getElementById("Moyenne_S1").value = moyenneS1.toFixed(2); // Arrondi à deux décimales
-        // Sélectionne automatiquement le résultat en fonction de la moyenne
-        if (moyenneS1 >= 10) {
-            document.getElementById("selectValue").value = "1"; // Admis (e)
-        } else {
-            document.getElementById("selectValue").value = "0"; // Ajourné (e)
-        }
+  if (!isNaN(totalS1)) {
+    var moyenneS1 = totalS1 / 30;
+    document.getElementById("Moyenne_S1").value = moyenneS1.toFixed(2); // Arrondi à deux décimales
+    // Sélectionne automatiquement le résultat en fonction de la moyenne
+    if (moyenneS1 >= 10) {
+      document.getElementById("selectValue").value = "1"; // Admis (e)
+    } else {
+      document.getElementById("selectValue").value = "0"; // Ajourné (e)
     }
+  }
 
-    if (!isNaN(totalS2)) {
-        var moyenneS2 = totalS2 / 30;
-        document.getElementById("Moyenne_S2").value = moyenneS2.toFixed(2); // Arrondi à deux décimales
-        // Sélectionne automatiquement le résultat en fonction de la moyenne
-        if (moyenneS2 >= 10) {
-            document.getElementById("selectValue2").value = "1"; // Admis (e)
-        } else {
-            document.getElementById("selectValue2").value = "0"; // Ajourné (e)
-        }
+  if (!isNaN(totalS2)) {
+    var moyenneS2 = totalS2 / 30;
+    document.getElementById("Moyenne_S2").value = moyenneS2.toFixed(2); // Arrondi à deux décimales
+    // Sélectionne automatiquement le résultat en fonction de la moyenne
+    if (moyenneS2 >= 10) {
+      document.getElementById("selectValue2").value = "1"; // Admis (e)
+    } else {
+      document.getElementById("selectValue2").value = "0"; // Ajourné (e)
     }
+  }
 }
 
 // Écoute les événements de saisie dans les champs Total_S1 et Total_S2
@@ -46,3 +46,14 @@ document.getElementById("Total_S2").addEventListener("input", updateForm);
 // Appel initial pour mettre à jour les moyennes et les résultats si des valeurs sont déjà présentes au chargement de la page
 updateForm();
 
+document.addEventListener("DOMContentLoaded", function () {
+  var acceptTermsCheckbox = document.getElementById("acceptTerms");
+  var submitButton = document.querySelector("button[type='submit']");
+
+  acceptTermsCheckbox.addEventListener("change", function () {
+    submitButton.disabled = !acceptTermsCheckbox.checked;
+  });
+
+  // Désactiver initialement le bouton de soumission
+  submitButton.disabled = true;
+});
